b44a7f5de7777fc84e6135057695f4dbbbb19ff7
tpowers23 <63263308+tpowers23@users.noreply.github.com>
2022-03-14 12:18:37 -0700

    Add files via upload

--- public/js/gamescene.js
+++ public/js/gamescene.js
@@ -64,7 +64,7 @@ export default class GameScene extends Phaser.Scene {
 
         this.socket.on('gameOver', (winner) => {
             let winnerText = winner + ' won!';
-            self.add.text(300,70,winnerText);
+            self.add.text(275,50,winnerText,{ fontSize: 36 });
         });
 
 
@@ -261,12 +261,12 @@ export default class GameScene extends Phaser.Scene {
                 }
                 else if (i > 3){
                     if (j < 3){
-                        if (col[j] === 'r' && this.getBoardFromCol(i-1)[j+1] === 'r' && this.getBoardFromCol(i-2)[j+2] === 'r' && this.getBoardFromCol(i-3)[j+3 === 'r']){
+                        if (col[j] === 'r' && this.getBoardFromCol(i-1)[j+1] === 'r' && this.getBoardFromCol(i-2)[j+2] === 'r' && this.getBoardFromCol(i-3)[j+3] === 'r') {
                             // red won diagonally
                             this.socket.emit('gameOver', 'red');
                             this.gameOver = true;
                         }
-                        else if (col[j] === 'y' && this.getBoardFromCol(i-1)[j+1] === 'y' && this.getBoardFromCol(i-2)[j+2] === 'y' && this.getBoardFromCol(i-3)[j+3 === 'y']){
+                        else if (col[j] === 'y' && this.getBoardFromCol(i-1)[j+1] === 'y' && this.getBoardFromCol(i-2)[j+2] === 'y' && this.getBoardFromCol(i-3)[j+3] === 'y') {
                             // yellow won diagonally
                             this.socket.emit('gameOver', 'yellow');
                             this.gameOver = true;
